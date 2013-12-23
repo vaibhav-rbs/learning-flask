f2e1163a5b80ada98af024c4d4380ffa45bd6449
vaibhav-rbs <vaibhav.rbs@gmail.com>
2013-12-23 00:55:28 +0000

    login

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from app import app
 from flask import render_template
+from forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -19,3 +20,10 @@ def index():
         title = 'Home',
         user = user,
         posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', 
+        title = 'Sign In',
+        form = form)
