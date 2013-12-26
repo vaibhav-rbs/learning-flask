7b090ff27834bb24e626fda43015addfab2c0c38
vb8188 <vb8188@vm-0.vb8188.koding.kd.io>
2013-12-26 00:18:04 +0000

    completed chapter3 of tutorial.

--- app/views.py
+++ app/views.py
@@ -4,7 +4,6 @@ from forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-@app.route('/authors')
 def index():
     user = { 'nickname': 'Welcome to my website' } # fake user
     posts = [ # fake array of posts
@@ -25,6 +24,10 @@ def index():
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', 
         title = 'Sign In',
-        form = form)
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
